35b81f1171f78c9200fa315baacf8c1a58dc8606
João Luca Ripardo <jlucartc@gmail.com>
2018-04-26 16:11:33 -0300

    Update PRAE.sql

--- PRAE.sql
+++ PRAE.sql
@@ -18,7 +18,7 @@ DROP TABLE IF EXISTS "categorias";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE "categorias" (
-  "id" int(10) unsigned NOT NULL,
+  "id" bigint NOT NULL,
   "nome" text COLLATE utf8mb4_unicode_ci NOT NULL,
   "responsavel" text COLLATE utf8mb4_unicode_ci,
   "email" text COLLATE utf8mb4_unicode_ci,
@@ -36,11 +36,10 @@ CREATE TABLE "categorias" (
 -- Dumping data for table "categorias"
 --
 
-LOCK TABLES "categorias" WRITE;
+LOCK TABLE "categorias";
 /*!40000 ALTER TABLE "categorias" DISABLE KEYS */;
 INSERT INTO "categorias" VALUES (1,'Auxilio Moradia','COORDENADORIA DE ASSISTÊNCIA ESTUDANTIL','prae@ufc.br','Benfica','Rua Paulino Nogueira','315 - Bloco III - 1° Andar','Benfica','2018-03-22 19:23:34','2018-03-28 19:47:34'),(2,'Categoria 2','---','---','---','---','---','---','2018-04-11 21:49:19','2018-04-11 21:49:19');
 /*!40000 ALTER TABLE "categorias" ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table "descricoes"
@@ -50,10 +49,10 @@ DROP TABLE IF EXISTS "descricoes";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE "descricoes" (
-  "id" int(10) unsigned NOT NULL,
+  "id" bigint NOT NULL,
   "titulo" text COLLATE utf8mb4_unicode_ci NOT NULL,
   "texto" text COLLATE utf8mb4_unicode_ci NOT NULL,
-  "categoria_id" int(11) NOT NULL,
+  "categoria_id" bigint NOT NULL,
   "created_at" timestamp NULL DEFAULT NULL,
   "updated_at" timestamp NULL DEFAULT NULL,
   PRIMARY KEY ("id")
@@ -64,11 +63,10 @@ CREATE TABLE "descricoes" (
 -- Dumping data for table "descricoes"
 --
 
-LOCK TABLES "descricoes" WRITE;
+LOCK TABLE "descricoes";
 /*!40000 ALTER TABLE "descricoes" DISABLE KEYS */;
 INSERT INTO "descricoes" VALUES (1,'Auxílio Moradia','O Programa Auxílio Moradia tem por objetivo viabilizar a permanência de estudantes matriculados nos Cursos de Graduação dos Campi da Universidade Federal do Ceará (UFC) em Sobral, Cariri e Quixadá, em comprovada situação de vulnerabilidade econômica, assegurando-lhes auxílio institucional para complementação de despesas com moradia e alimentação durante todo o período do curso ou enquanto persistir a mesma situação.\r\n\r\nImportante! A vinculação dos estudantes ao Programa Auxílio Moradia não os impede de receber, por mérito, qualquer uma das bolsas dos diversos programas da UFC, de agências de fomento ou de empresas.',1,'2018-03-22 21:06:07','2018-03-28 19:47:13');
 /*!40000 ALTER TABLE "descricoes" ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table "documentos"
@@ -78,8 +76,8 @@ DROP TABLE IF EXISTS "documentos";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE "documentos" (
-  "id" int(10) unsigned NOT NULL,
-  "categoria_id" int(11) NOT NULL,
+  "id" bigint NOT NULL,
+  "categoria_id" bigint NOT NULL,
   "rota" text COLLATE utf8mb4_unicode_ci NOT NULL,
   "nome" text COLLATE utf8mb4_unicode_ci NOT NULL,
   "created_at" timestamp NULL DEFAULT NULL,
@@ -92,11 +90,10 @@ CREATE TABLE "documentos" (
 -- Dumping data for table "documentos"
 --
 
-LOCK TABLES "documentos" WRITE;
+LOCK TABLE "documentos";
 /*!40000 ALTER TABLE "documentos" DISABLE KEYS */;
 INSERT INTO "documentos" VALUES (7,1,'1/Exemplo','Exemplo','2018-04-10 21:36:24','2018-04-10 21:36:24'),(8,1,'1/Relatorio','Relatorio','2018-04-10 21:37:27','2018-04-10 21:37:27');
 /*!40000 ALTER TABLE "documentos" ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table "migrations"
@@ -106,9 +103,9 @@ DROP TABLE IF EXISTS "migrations";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE "migrations" (
-  "id" int(10) unsigned NOT NULL,
+  "id" bigint NOT NULL,
   "migration" varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  "batch" int(11) NOT NULL,
+  "batch" integer NOT NULL,
   PRIMARY KEY ("id")
 );
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -117,11 +114,10 @@ CREATE TABLE "migrations" (
 -- Dumping data for table "migrations"
 --
 
-LOCK TABLES "migrations" WRITE;
+LOCK TABLE "migrations";
 /*!40000 ALTER TABLE "migrations" DISABLE KEYS */;
 INSERT INTO "migrations" VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2018_03_15_194109_create_categorias_table',1),(4,'2018_03_20_183411_create_descricoes_table',1),(5,'2018_03_20_183457_create_documentos_table',1);
 /*!40000 ALTER TABLE "migrations" ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table "password_resets"
@@ -142,10 +138,9 @@ CREATE TABLE "password_resets" (
 -- Dumping data for table "password_resets"
 --
 
-LOCK TABLES "password_resets" WRITE;
+LOCK TABLE "password_resets";
 /*!40000 ALTER TABLE "password_resets" DISABLE KEYS */;
 /*!40000 ALTER TABLE "password_resets" ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table "usuarios"
@@ -155,7 +150,7 @@ DROP TABLE IF EXISTS "usuarios";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE "usuarios" (
-  "id" int(10) unsigned NOT NULL,
+  "id" bigint NOT NULL,
   "nome" varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   "email" varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   "usuario" varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -164,7 +159,7 @@ CREATE TABLE "usuarios" (
   "created_at" timestamp NULL DEFAULT NULL,
   "updated_at" timestamp NULL DEFAULT NULL,
   PRIMARY KEY ("id"),
-  UNIQUE KEY "usuarios_email_unique" ("email")
+  KEY "usuarios_email_unique" ("email")
 );
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -172,11 +167,10 @@ CREATE TABLE "usuarios" (
 -- Dumping data for table "usuarios"
 --
 
-LOCK TABLES "usuarios" WRITE;
+LOCK TABLE "usuarios";
 /*!40000 ALTER TABLE "usuarios" DISABLE KEYS */;
 INSERT INTO "usuarios" VALUES (1,'admin','admin@admin','admin','$2y$10$/FzOcCGm4TtleSbiFFc.euHN5.hXBwVe2N7dcUqOY5j5TrZch3586','8A8tp6CWNt3HpzKugJwMFxW0aj0KkE4aQfc6ADiGwd7Hh4kNuQuUm7g3vA5h','2018-03-20 22:34:17','2018-03-20 22:34:17'),(3,'Usuario 22','user2@user2','User2','$2y$10$SQFtaVA8tfV99eyOIvorK.Du/aCY0nagPqQgwOm6mS/wADeDEwn.q',NULL,'2018-03-27 22:19:02','2018-03-27 22:49:32');
 /*!40000 ALTER TABLE "usuarios" ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
