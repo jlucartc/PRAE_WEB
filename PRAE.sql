94487b2210de22c32515284c84def499e13116e2
João Luca Ripardo <jlucartc@gmail.com>
2018-04-26 16:16:24 -0300

    Update PRAE.sql

--- PRAE.sql
+++ PRAE.sql
@@ -35,10 +35,11 @@ CREATE TABLE "categorias" (
 --
 -- Dumping data for table "categorias"
 --
-
+BEGIN;
 LOCK TABLE "categorias";
 /*!40000 ALTER TABLE "categorias" DISABLE KEYS */;
 INSERT INTO "categorias" VALUES (1,'Auxilio Moradia','COORDENADORIA DE ASSISTÊNCIA ESTUDANTIL','prae@ufc.br','Benfica','Rua Paulino Nogueira','315 - Bloco III - 1° Andar','Benfica','2018-03-22 19:23:34','2018-03-28 19:47:34'),(2,'Categoria 2','---','---','---','---','---','---','2018-04-11 21:49:19','2018-04-11 21:49:19');
+COMMIT;
 /*!40000 ALTER TABLE "categorias" ENABLE KEYS */;
 
 --
@@ -63,9 +64,11 @@ CREATE TABLE "descricoes" (
 -- Dumping data for table "descricoes"
 --
 
+BEGIN;
 LOCK TABLE "descricoes";
 /*!40000 ALTER TABLE "descricoes" DISABLE KEYS */;
 INSERT INTO "descricoes" VALUES (1,'Auxílio Moradia','O Programa Auxílio Moradia tem por objetivo viabilizar a permanência de estudantes matriculados nos Cursos de Graduação dos Campi da Universidade Federal do Ceará (UFC) em Sobral, Cariri e Quixadá, em comprovada situação de vulnerabilidade econômica, assegurando-lhes auxílio institucional para complementação de despesas com moradia e alimentação durante todo o período do curso ou enquanto persistir a mesma situação.\r\n\r\nImportante! A vinculação dos estudantes ao Programa Auxílio Moradia não os impede de receber, por mérito, qualquer uma das bolsas dos diversos programas da UFC, de agências de fomento ou de empresas.',1,'2018-03-22 21:06:07','2018-03-28 19:47:13');
+COMMIT;
 /*!40000 ALTER TABLE "descricoes" ENABLE KEYS */;
 
 --
@@ -90,9 +93,11 @@ CREATE TABLE "documentos" (
 -- Dumping data for table "documentos"
 --
 
+BEGIN;
 LOCK TABLE "documentos";
 /*!40000 ALTER TABLE "documentos" DISABLE KEYS */;
 INSERT INTO "documentos" VALUES (7,1,'1/Exemplo','Exemplo','2018-04-10 21:36:24','2018-04-10 21:36:24'),(8,1,'1/Relatorio','Relatorio','2018-04-10 21:37:27','2018-04-10 21:37:27');
+COMMIT;
 /*!40000 ALTER TABLE "documentos" ENABLE KEYS */;
 
 --
@@ -114,9 +119,11 @@ CREATE TABLE "migrations" (
 -- Dumping data for table "migrations"
 --
 
+BEGIN;
 LOCK TABLE "migrations";
 /*!40000 ALTER TABLE "migrations" DISABLE KEYS */;
 INSERT INTO "migrations" VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2018_03_15_194109_create_categorias_table',1),(4,'2018_03_20_183411_create_descricoes_table',1),(5,'2018_03_20_183457_create_documentos_table',1);
+COMMIT;
 /*!40000 ALTER TABLE "migrations" ENABLE KEYS */;
 
 --
@@ -138,7 +145,7 @@ CREATE TABLE "password_resets" (
 -- Dumping data for table "password_resets"
 --
 
-LOCK TABLE "password_resets";
+/*LOCK TABLE "password_resets";*/
 /*!40000 ALTER TABLE "password_resets" DISABLE KEYS */;
 /*!40000 ALTER TABLE "password_resets" ENABLE KEYS */;
 
@@ -167,9 +174,11 @@ CREATE TABLE "usuarios" (
 -- Dumping data for table "usuarios"
 --
 
+BEGIN;
 LOCK TABLE "usuarios";
 /*!40000 ALTER TABLE "usuarios" DISABLE KEYS */;
 INSERT INTO "usuarios" VALUES (1,'admin','admin@admin','admin','$2y$10$/FzOcCGm4TtleSbiFFc.euHN5.hXBwVe2N7dcUqOY5j5TrZch3586','8A8tp6CWNt3HpzKugJwMFxW0aj0KkE4aQfc6ADiGwd7Hh4kNuQuUm7g3vA5h','2018-03-20 22:34:17','2018-03-20 22:34:17'),(3,'Usuario 22','user2@user2','User2','$2y$10$SQFtaVA8tfV99eyOIvorK.Du/aCY0nagPqQgwOm6mS/wADeDEwn.q',NULL,'2018-03-27 22:19:02','2018-03-27 22:49:32');
+COMMIT;
 /*!40000 ALTER TABLE "usuarios" ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
