f1d0c5c33fb91a9ba05665dc5af861d2a000356a
jlucartc <jlucartc@gmail.com>
2018-05-02 15:40:41 -0300

    Tentando consertar problema de conteudo misto (HTTP/HTTPS)

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,7 +1,7 @@
 @if(!Auth::check())
 <nav class="navbar navbar-expand-sm navbar-light bg-white sticky-top">
-  <a href="{{ route('index') }}" class='navbar-brand'> <img  width='40px' height='40px' src="{{ asset('ufc.png') }}" alt=""> </a>
-  <form class="form-inline mr-0 ml-5" action="{{ route('login')}}" method="post">
+  <a href="{{ secure_url(route('index')) }}" class='navbar-brand'> <img  width='40px' height='40px' src="{{ secure_asset('ufc.png') }}" alt=""> </a>
+  <form class="form-inline mr-0 ml-5" action="{{ secure_url(route('login')) }}" method="post">
     {{ csrf_field() }}
     <input class="form-control mr-2" type="text" name="usuario" value="" placeholder="Digite seu usuário">
     <input class="form-control mr-3" type="password" name="senha" value="" placeholder="Digite sua senha">
@@ -10,11 +10,11 @@
 </nav>
 @else
 <nav class="navbar navbar-expand-sm navbar-light bg-white fixed-top">
-  <a href="{{ route('usuario.categorias') }}" class='navbar-brand'> <img  width='40px' height='40px' src="{{ asset('ufc.png') }}" alt=""> </a>
+  <a href="{{ secure_url(route('usuario.categorias')) }}" class='navbar-brand'> <img  width='40px' height='40px' src="{{ secure_asset('ufc.png') }}" alt=""> </a>
   <div class="navbar-nav">
-    <a href="{{ route('usuario.categorias') }}" class="nav-item nav-link">Categorias</a>
-    <a href="{{ route('usuario.usuarios') }}" class="nav-item nav-link">Usuários</a>
-    <a href="{{ route('logout') }}" class="nav-item nav-link ml-5">Logout</a>
+    <a href="{{ secure_url(route('usuario.categorias')) }}" class="nav-item nav-link">Categorias</a>
+    <a href="{{ secure_url(route('usuario.usuarios')) }}" class="nav-item nav-link">Usuários</a>
+    <a href="{{ secure_url(route('logout')) }}" class="nav-item nav-link ml-5">Logout</a>
   </div>
 </nav>
 @endif
