a5919db53df44c4ccf1d7a3ad10bfb080b434ada
jlucartc <jlucartc@gmail.com>
2018-05-08 14:37:45 -0300

    secure_asset -> asset

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,13 +2,13 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/all.css') }}">
     <title>@yield('titulo')</title>
   </head>
   <body>
     @yield('conteudo')
   </body>
-  <script type="text/javascript" src="{{ secure_asset('js/app.js') }}"></script>
-  <script type="text/javascript" src="{{ secure_asset('js/all.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/all.js') }}"></script>
 </html>
